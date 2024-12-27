9b2217ab695ad537c84852f25d535bea885fd3ea
jola-ds <jolajayy@gmail.com>
2024-12-27 14:36:29 +0100

    updated module header and docstring

--- 5_tdd_with_llms/exercises/sort_numbers.py
+++ 5_tdd_with_llms/exercises/sort_numbers.py
@@ -4,7 +4,7 @@ A module for sorting a list of numbers in ascending order.
 
 Module Contents:
 
-    - sort_numbers(l: list) -> list:
+    - sort_numbers(lst: list) -> list:
     Sorts numbers in a list from lowest to highest.
 
 Created on: 2024-12-19
@@ -17,13 +17,13 @@ def sort_numbers(lst: list) -> list:
     Returns a list of numbers sorted in ascending order.
 
     Parameters:
-        l: list, an unordered list of numbers to be sorted
+        lst: list, an unordered list of numbers to be sorted
 
     Returns -> list, a list of numbers sorted from lowest to highest.
 
     Raises:
-        AssertionError: if l is not a list
-        AssertionError: if elements in l are not integers or floats
+        AssertionError: if lst is not a list
+        AssertionError: if elements in lst are not integers or floats
 
     >>> sort_numbers([7, 2, 3, 6])
     [2, 3, 6, 7]
@@ -49,5 +49,3 @@ def sort_numbers(lst: list) -> list:
                 l_copy[j], l_copy[j + 1] = l_copy[j + 1], l_copy[j]
 
     return l_copy
-
-print(sort_numbers([2, 1, 2]))
